8f5e04d382b2bc1597236566a79d647649fd8e96
J Brian Watkins <bwatkins@fresh.local>
2016-02-16 16:38:54 -0500

    Updates to deploy app on pcf
    - set ruby version to 2.2.3 in Gemfile
    - updated seed data with more tags
    - added a manifest file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,13 @@
-seed_file = Rails.root.join("db/seeds/#{Rails.env}.rb")
-if seed_file.exist? && !ENV['NO_SEED_DATA']
-  puts "*** Loading #{Rails.env} seed data"
-  require seed_file
+channels = %w(vim development design consulting rails ruby testing bash git
+              html-css javascript angular sql devops internet java spring intellij security)
+
+channels.each do |channel|
+  puts "Finding or creating channel: #{channel}"
+  Channel.find_or_create_by!(name: channel)
 end
+
+# seed_file = Rails.root.join("db/seeds/#{Rails.env}.rb")
+# if seed_file.exist? && !ENV['NO_SEED_DATA']
+#   puts "*** Loading #{Rails.env} seed data"
+#   require seed_file
+# end
